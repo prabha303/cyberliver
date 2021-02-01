8ccc4ebee69d631a19f01b81f60b8b767c3c4b55
Pruse <purushotham@tranzo.ai>
2021-02-01 11:22:35 +0530

    Log activity is added

---
+++ alcochange-dtx/models/logActivity.go
@@ -0,0 +1,24 @@
+package models
+
+import (
+	"ecargoware/alcochange-dtx/dbcon"
+	"time"
+)
+
+// Activity logger
+type APILogActivity struct {
+	ID          int64       `json:"id"`
+	UserID      int64       `json:"userid"`
+	User        string      `json:"user"`
+	Resource    string      `json:"resource"`
+	Action      string      `json:"action"`
+	RequestBody interface{} `json:"requestBody"`
+	Response    interface{} `json:"response"`
+	CreatedAt   time.Time   `json:"createdAt"`
+}
+
+func SaveAPILogActivity(api *APILogActivity) {
+
+	db := dbcon.Get()
+	db.Model(api).Insert()
+}
